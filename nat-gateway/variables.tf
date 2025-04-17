0d32552562d5262782b949318f878354fbd4dcc9
Your Name <az.okolo.2084@gmail.com>
2025-04-17 23:34:42 +0100

    created nat-gateway module

---
+++ nat-gateway/variables.tf
@@ -0,0 +1,11 @@
+# nat-gateway variables
+variable "project_name" {}
+variable "environment" {}
+variable "public_subnet_az1" {}
+variable "internet_gateway" {}
+variable "public_subnet_az2" {}
+variable "vpc_id" {}
+variable "private_app_subnet_az1" {}
+variable "private_data_subnet_az1" {}
+variable "private_app_subnet_az2" {}
+variable "private_data_subnet_az2" {}
