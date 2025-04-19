1b787b12eae86c070b73840884975c7e6a98fd7f
Your Name <az.okolo.2084@gmail.com>
2025-04-19 01:36:48 +0100

    created rds module

---
+++ rds/variables.tf
@@ -0,0 +1,10 @@
+# rds variables
+variable "project_name" {}
+variable "environment" {}
+variable "private_data_subnet_az1" {}
+variable "private_data_subnet_az2" {}
+variable "database_snapshot_identifier" {}
+variable "database_instance_class" {}
+variable "availability_zone_1" {}
+variable "database_instance_identifier" {}
+variable "multi_az_deployment" {}
